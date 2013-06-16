f23370b294ec56fcaa02b225d2c2526119dc6994
Benny <user@server>
2013-06-15 23:42:57 -0400

    Updated for tutorial 4

--- src/com/base/engine/Vector2f.java
+++ src/com/base/engine/Vector2f.java
@@ -11,24 +11,97 @@ public class Vector2f
 		this.y = y;
 	}
 
+	public float length()
+	{
+		return (float)Math.sqrt(x * x + y * y);
+	}
+	
+	public float dot(Vector2f r)
+	{
+		return x * r.getX() + y * r.getY();
+	}
+	
+	public Vector2f normalize()
+	{
+		float length = length();
+		
+		x /= length;
+		y /= length;
+		
+		return this;
+	}
+	
+	public Vector2f rotate(float angle)
+	{
+		double rad = Math.toRadians(angle);
+		double cos = Math.cos(rad);
+		double sin = Math.sin(rad);
+		
+		return new Vector2f((float)(x * cos - y * sin),(float)(x * sin + y * cos));
+	}
+	
+	public Vector2f add(Vector2f r)
+	{
+		return new Vector2f(x + r.getX(), y + r.getY());
+	}
+	
+	public Vector2f add(float r)
+	{
+		return new Vector2f(x + r, y + r);
+	}
+	
+	public Vector2f sub(Vector2f r)
+	{
+		return new Vector2f(x - r.getX(), y - r.getY());
+	}
+	
+	public Vector2f sub(float r)
+	{
+		return new Vector2f(x - r, y - r);
+	}
+	
+	public Vector2f mul(Vector2f r)
+	{
+		return new Vector2f(x * r.getX(), y * r.getY());
+	}
+	
+	public Vector2f mul(float r)
+	{
+		return new Vector2f(x * r, y * r);
+	}
+	
+	public Vector2f div(Vector2f r)
+	{
+		return new Vector2f(x / r.getX(), y / r.getY());
+	}
+	
+	public Vector2f div(float r)
+	{
+		return new Vector2f(x / r, y / r);
+	}
+	
 	public String toString()
 	{
 		return "(" + x + " " + y + ")";
 	}
 	
-	public float getX() {
+	public float getX() 
+	{
 		return x;
 	}
 
-	public void setX(float x) {
+	public void setX(float x) 
+	{
 		this.x = x;
 	}
 
-	public float getY() {
+	public float getY() 
+	{
 		return y;
 	}
 
-	public void setY(float y) {
+	public void setY(float y) 
+	{
 		this.y = y;
 	}
 }
---
+++ src/com/base/engine/Vector3f.java
@@ -0,0 +1,114 @@
+package com.base.engine;
+
+public class Vector3f 
+{
+	private float x;
+	private float y;
+	private float z;
+	
+	public Vector3f(float x, float y, float z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public float length()
+	{
+		return (float)Math.sqrt(x * x + y * y + z * z);
+	}
+	
+	public float dot(Vector3f r)
+	{
+		return x * r.getX() + y * r.getY() + z * r.getZ();
+	}
+	
+	public Vector3f cross(Vector3f r)
+	{
+		float x_ = y * r.getZ() - z * r.getY();
+		float y_ = z * r.getX() - x * r.getZ();
+		float z_ = x * r.getY() - y * r.getX();
+		
+		return new Vector3f(x_, y_, z_);
+	}
+	
+	public Vector3f normalize()
+	{
+		float length = length();
+		
+		x /= length;
+		y /= length;
+		z /= length;
+		
+		return this;
+	}
+	
+	public Vector3f rotate()
+	{
+		return null;
+	}
+	
+	public Vector3f add(Vector3f r)
+	{
+		return new Vector3f(x + r.getX(), y + r.getY(), z + r.getZ());
+	}
+	
+	public Vector3f add(float r)
+	{
+		return new Vector3f(x + r, y + r, z + r);
+	}
+	
+	public Vector3f sub(Vector3f r)
+	{
+		return new Vector3f(x - r.getX(), y - r.getY(), z - r.getZ());
+	}
+	
+	public Vector3f sub(float r)
+	{
+		return new Vector3f(x - r, y - r, z - r);
+	}
+	
+	public Vector3f mul(Vector3f r)
+	{
+		return new Vector3f(x * r.getX(), y * r.getY(), z * r.getZ());
+	}
+	
+	public Vector3f mul(float r)
+	{
+		return new Vector3f(x * r, y * r, z * r);
+	}
+	
+	public Vector3f div(Vector3f r)
+	{
+		return new Vector3f(x / r.getX(), y / r.getY(), z / r.getZ());
+	}
+	
+	public Vector3f div(float r)
+	{
+		return new Vector3f(x / r, y / r, z / r);
+	}
+	
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+}
