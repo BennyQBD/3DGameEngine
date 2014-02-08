dea1022823eac150803dc12404163db5fbfd44d2
BennyQBD <bennyqbdspam@gmail.com>
2014-02-08 11:05:36 -0500

    Updated for tutorial 37

--- src/com/base/engine/core/Matrix4f.java
+++ src/com/base/engine/core/Matrix4f.java
@@ -85,13 +85,13 @@ public class Matrix4f
 
 	public Matrix4f initOrthographic(float left, float right, float bottom, float top, float near, float far)
 	{
-		float halfWidth = (right - left)/2.0f;
-		float halfHeight = (top - bottom)/2.0f;
-		float halfDepth = (far - near)/2.0f;
+		float width = right - left;
+		float height = top - bottom;
+		float depth = far - near;
 
-		m[0][0] = halfWidth;m[0][1] = 0;	m[0][2] = 0;	m[0][3] = Math.abs(halfWidth);
-		m[1][0] = 0;	m[1][1] = halfHeight;m[1][2] = 0;	m[1][3] = Math.abs(halfHeight);
-		m[2][0] = 0;	m[2][1] = 0;	m[2][2] = halfDepth;m[2][3] = Math.abs(halfDepth);
+		m[0][0] = 2/width;m[0][1] = 0;	m[0][2] = 0;	m[0][3] = -(right + left)/width;
+		m[1][0] = 0;	m[1][1] = 2/height;m[1][2] = 0;	m[1][3] = -(top + bottom)/height;
+		m[2][0] = 0;	m[2][1] = 0;	m[2][2] = -2/depth;m[2][3] = -(far + near)/depth;
 		m[3][0] = 0;	m[3][1] = 0;	m[3][2] = 0;	m[3][3] = 1;
 
 		return this;
