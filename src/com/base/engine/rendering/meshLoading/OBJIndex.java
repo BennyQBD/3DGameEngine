67f83f4bc293ef0244d5c4cf29215b7a9698a2e4
BennyQBD <bennyqbdspam@gmail.com>
2014-03-15 05:01:29 -0400

    Updated for tutorial 51

--- src/com/base/engine/rendering/meshLoading/OBJIndex.java
+++ src/com/base/engine/rendering/meshLoading/OBJIndex.java
@@ -1,8 +1,24 @@
 package com.base.engine.rendering.meshLoading;
 
-public class OBJIndex
+public class OBJIndex implements Comparable<OBJIndex>
 {
 	public int vertexIndex;
 	public int texCoordIndex;
 	public int normalIndex;
+	public int orderTag;
+
+	@Override
+	public int compareTo(OBJIndex objIndex)
+	{
+		final int BEFORE = -1;
+		final int AFTER = 1;
+		final int EQUAL = 0;
+
+		if(vertexIndex == objIndex.vertexIndex)
+			return EQUAL;
+		else if(vertexIndex < objIndex.vertexIndex)
+			return BEFORE;
+
+		return AFTER;
+	}
 }
